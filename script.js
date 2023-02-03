3d6003c1b9091cc63647abe919f154505f69287d
Zak <zakirali1@hotmail.co.uk>
2023-02-03 13:16:29 +0000

    changed API, output fields added

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // $(document).ready(function() {
 
 let search = $("#search-input")
+let today = $("#today");
 //  .val()
 //  .trim();
 
  console.log(search);
+ let todaysDate = moment().format(" \(d/D/YYYY)")
+ console.log(todaysDate)
 
 //  localStorage.setItem("city name", JSON.stringify(search));
 // });
@@ -18,7 +21,7 @@ localStorage.setItem("city name", JSON.stringify(searchVal));
 $("search-input").val(localStorage.getItem("city name"));
 // localStorage.getItem("city name")
 
-let queryURL = "http://api.openweathermap.org/geo/1.0/direct?q="
+let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="
 
 let api = "&appid=6a1c1f77dd62fe1efb6093d9ffc4bee5"
 
@@ -27,8 +30,15 @@ $.ajax({
     method: "GET"
 }).then(function(response) {
     console.log(response)
-
     
+    let icon = response.list[0].weather[0].icon;
+    console.log(icon);
+    let weatherIcon = $("<img>").attr("src", `http://openweathermap.org/img/wn/${icon}@2x.png`)
+
+    let cityN = $('<h1>').text(response.city.name + todaysDate);
+    cityN.append(weatherIcon)
+    $('#today').append(cityN)
+
 });
 
 });
